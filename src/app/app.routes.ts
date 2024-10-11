cdd25950e9c8b32f4d1200dd73cfd26f08fbff02
Dan1ell1 <daniellisz.silva@gmail.com>
2024-10-11 07:35:44 -0300

    projeto inteiro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'navbar', component: NavbarComponent},
+    { path: 'home', component: HomeComponent}
+];
